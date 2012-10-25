69d0685cc299fc61bc950bd6ee5ded7d692a46bc
Makara Wang <makara15@gmail.com>
2012-10-25 18:47:20 +0800

    Init.

--- _includes/init.js
+++ _includes/init.js
@@ -1,2 +1,14 @@
 // Initialize
 // ----------
+window.app = {
+    models: {},
+    views: {},
+    routers: {},
+    templates: _($('script[name]')).reduce(function(memo, el) {
+        memo[el.getAttribute('name')] = _(el.innerHTML).template();
+        return memo;
+    }, {}),
+    state: {},
+    instance: {}
+};
+window.args = _(window.app).toArray();
